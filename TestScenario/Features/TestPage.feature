e85c47ba2bd04725244d6dda79f7f936ef148908
4izzz <chizzz777@gmail.com>
2023-10-16 19:02:38 -0400

    Add project files.

---
+++ TestScenario/Features/TestPage.feature
@@ -0,0 +1,10 @@
+﻿Feature: TestPage
+
+A short summary of the feature
+
+@testTag
+Scenario: Add n elements
+	Given Open browser
+	When Navigate to URL
+	Then Add '8' elements
+	Then Assert that '8' number of elements exist on the page
---
+++ TestScenario/StepDefinitions/TestPageStepDefinitions.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using TechTalk.SpecFlow.Infrastructure;
+
+namespace TestScenario.StepDefinitions
+{
+    [Binding]
+    public sealed class TestPageStepDefinitions
+    {
+        private IWebDriver driver;
+
+        public TestPageStepDefinitions(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        [Given(@"Open browser")]
+        public void GivenOpenBrowser()
+        {
+            //driver = new ChromeDriver();
+        }
+
+        [When(@"Navigate to URL")]
+        public void WhenNavigateToURL()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Url = "http://localhost:7080/add_remove_elements/";//"https://the-internet.herokuapp.com/add_remove_elements/";
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@id='content']")));
+        }
+
+        [Then(@"Add '([^']*)' elements")]
+        public void ThenAddElements(string s)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+            var addElementButton = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//button[@onclick='addElement()']")));
+
+            int n = Int32.Parse(s);
+            for (int i = 0; i < n; i++)
+            {
+                addElementButton.Click();
+            }
+        }
+
+        [Then(@"Assert that '([^']*)' number of elements exist on the page")]
+        public void ThenAssertThatNumberOfElementsExistOnThePage(string s)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+
+            var elements = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@id='elements']")));
+            string childsCount = elements.GetAttribute("childElementCount");
+            
+            Assert.AreEqual(s, childsCount);
+        }
+
+    }
+}
